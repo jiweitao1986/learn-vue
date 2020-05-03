8a3220f2e73bcffd60bd5baaaaf7d3799d241641
Witt <jiwt@inspur.com>
2020-05-03 21:36:50 +0800

    验证Vue应用初始化的另外一种方式。

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+
+// $mount可以使用$mount代替
+// render接收的参数是一个createElement方法定义
+// new Vue({
+//   el: '#app',
+//   render: function(createElement) {
+//     return createElement(App);
+//   }
+// })
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
